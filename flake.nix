fc0ece3886afb9c9ca07291cdca217106ec367cb
pxzc1 <tonaok2555@gmail.com>
2026-02-15 20:35:26 +0700

    initials

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Python 3.12 + CUDA";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      python = pkgs.python312;
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "torch-shell";
+
+        packages = with pkgs; [
+          python
+          python.pkgs.pip
+          python.pkgs.virtualenv
+          zlib
+          stdenv.cc.cc
+          glibc
+          cudaPackages.cudatoolkit #cuda
+        ];
+
+        shellHook = ''
+          export venvDir=env
+          
+          export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib:${pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc
+            pkgs.zlib
+            pkgs.cudaPackages.cudatoolkit
+          ]}:$LD_LIBRARY_PATH"
+
+          if [ ! -d "$venvDir" ]; then
+            echo "Creating Virtual Environment in $venvDir"
+            ${python}/bin/python -m venv "$venvDir"
+          fi
+
+          source "$venvDir/bin/activate"
+
+          echo "Python: $(python --version)"
+          
+          python verifications.py
+        '';
+      };
+    };
+}
