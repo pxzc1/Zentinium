dde61650beb1858ec0fb96643471b67d2c6857c7
pxzc1 <tonaok2555@gmail.com>
2026-03-07 01:16:30 +0700

    vite root -> web

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src/ui',
+  root: 'web',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
