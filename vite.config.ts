21c958c4340eedbd50e762b7819cd4590a74ab57
pxzc1 <tonaok2555@gmail.com>
2026-03-07 01:15:18 +0700

    life is good

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'web',
+  root: 'src/ui',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
